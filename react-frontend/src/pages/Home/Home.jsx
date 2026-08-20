9cbeb36310cfdeb925e9ca667570c70689a4c95d
giulikarime <giulianakarimedl@gmail.com>
2026-08-20 16:56:09 -0300

    feature/login: Styling Home with css.

--- react-frontend/src/pages/Home/Home.jsx
+++ react-frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
+import burger_menu from "../../assets/header/burger_menu-nobg.svg";
+import logo from '../../assets/Tambaki_Prototype.png'
+import bell from '../../assets/header/notification_bell-nobg.svg'
+import user from '../../assets/header/user_profile-nobg.svg'
+import './home.css';
 
 function Home() {
     const [userName, setUserName] = useState("");
+    const [expanded,setExpand] = useState(false);
 
     useEffect(() => {
         const account = JSON.parse(localStorage.getItem("account"));
@@ -11,7 +17,28 @@ function Home() {
     }, []);
 
     return (
-        <h1>Olá, {userName}</h1>
+        <main>
+            <header>
+                <div id="group-one">
+                    <button onClick={()=>setExpand(!expanded)}>
+                        <img src={burger_menu} alt="" />
+                    </button>
+                    <img src={logo} alt="" />
+                    <h1>Olá, {userName} </h1>
+                </div>
+                <input type="search" name="" id="" placeholder="Buscar por pratos, bebidas, usuários..."/>
+                <div id="group-two">
+                    <button><img src={bell} alt="" /></button>
+                    <button><img style={{backgroundColor: '#87b3f2', borderRadius: "50px", padding:"4px"}} src={user} alt="" />
+</button>
+                </div>
+            </header>
+            {expanded && (
+                <div id="sidebar">
+
+                </div>
+            )}
+        </main>
     );
 }
 
