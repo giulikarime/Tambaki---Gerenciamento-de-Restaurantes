ceece5e5e5fb44e13ba9e109c706a4ab9936fd77
Julia <julia.resplandes@aluno.senai.br>
2026-08-21 14:49:04 -0300

    feature/configuration: Add Configuration page and update routing

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
-import Login from './pages/FirstAccess/login';
+import Login from './pages/FirstAccess/Login.jsx';
 import Home from './pages/Home/Home';
+import Configuration from './pages/Configuration/Configuration.jsx';
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}></Route>
         <Route path='/dashboard' element={<Home/>}></Route>
+        <Route path='/configuration' element={<Configuration/>}></Route>
       </Routes>
     </main>
   );
---
+++ react-frontend/src/pages/Configuration/Configuration.jsx
@@ -0,0 +1,12 @@
+import './configuration.css'
+
+function Configuration(){
+    return(
+        <main>
+            <h1>Configuração</h1>
+            
+                    </main>
+    )
+}
+
+export default Configuration;
